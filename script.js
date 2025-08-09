5833698a21d6a56ab7bb1589e25cf87a759e3df6
Mohd Faizan <mohdfaizan63985@gmail.com>
2025-08-09 12:12:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputbx = document.getElementById("inpbx");
+const listItem = document.getElementById("litem");
+
+function toAdd(){
+ if(inputbx.value === '') alert("You must write something");
+
+        else{
+            let li = document.createElement('li');
+            li.innerHTML = inputbx.value;
+            listItem.appendChild(li);
+            let span = document.createElement('span');
+            span.innerHTML = "\u00d7"
+            li.appendChild(span);
+            saveData();
+    }
+    inputbx.value = '';
+}       
+
+listItem.addEventListener("click", (e)=>{
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listItem.innerHTML);
+
+}
+
+function showTask(){
+    listItem.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
